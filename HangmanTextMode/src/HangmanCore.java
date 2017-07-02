f90b7e5887e392402da0c6e4fb804038f97f442d
Luis Miguens <luis.miguens@gmail.com>
2017-07-02 11:57:16 +0200

    First draft of Hangman in text mode, with some bugs and room for improvement

--- HangmanTextMode/src/HangmanCore.java
+++ HangmanTextMode/src/HangmanCore.java
@@ -1,4 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+enum STATE {PLAYING, GAME_OVER, WIN };
 
 public class HangmanCore {
-
+	private STATE m_state;
+	private String m_word;
+	private List<Character> m_playedLetters;
+	private int m_triesLeft;
+	
+	private HangmanCore(String word, int nTries)
+	{
+		m_state = STATE.PLAYING;
+		m_word = word.toUpperCase();
+		m_playedLetters = new ArrayList<Character>();
+		m_triesLeft = nTries;
+	}
+	
+	public HangmanCore()
+	{
+		this("test", 8);
+	}
+	
+	public int triesLeft() 
+	{ 
+		return m_triesLeft; 
+	}
+	
+	public void play(Character letter)
+	{
+		if(Character.isLetter(letter) == false)
+			return; // Do nothing
+		
+		letter = Character.toUpperCase(letter);
+		
+		if(m_playedLetters.contains(letter) == false)
+			m_playedLetters.add(letter);
+			if (m_word.indexOf(letter) ==  -1) // -1 means not found
+			{
+				m_triesLeft--;
+				updateState();
+			}
+			
+	}
+	
+	public STATE state()
+	{
+		return m_state;
+	}
+	
+	public String word()
+	{
+		String word = " ";
+		for (Character letter: m_word.toCharArray())
+			if(m_playedLetters.contains(letter) == true)
+				word += letter.toString() + ' ';
+			else
+				word += Character.toString('_') + ' ';
+		return word;
+	}
+	
+	private void updateState()
+	{
+		if(m_triesLeft == 0)
+			m_state = STATE.GAME_OVER;
+		
+		boolean everythingFound = true;
+		for(Character letter: m_word.toCharArray())
+		{
+			if(m_playedLetters.contains(letter) == false)
+			{
+				everythingFound = false;
+				break;
+			}
+		}
+		
+		if(everythingFound)
+			m_state = STATE.WIN;
+	}
 }
--- HangmanTextMode/src/HangmanGame.java
+++ HangmanTextMode/src/HangmanGame.java
@@ -1,9 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HangmanGame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		
+		HangmanCore game = new HangmanCore();
 
+		while(game.state() == STATE.PLAYING)
+		{
+			System.out.println("Word:" + game.word());
+			System.out.println("Tries Left: " + game.triesLeft());
+			System.out.print("[letter] ? ");
+			String line = bufferedReader.readLine();
+			game.play(line.charAt(0));
+
+			switch (game.state()) {
+			case GAME_OVER:
+				System.out.println("GAME OVER");
+				System.out.println("Word:" + game.word());
+				break;
+			case WIN:
+				System.out.println("You win!");
+				System.out.println("Word:" + game.word());
+				break;
+			default:
+				// do nothing
+			}
+		}
 	}
-
 }
