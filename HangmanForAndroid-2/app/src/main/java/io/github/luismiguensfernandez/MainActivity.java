111d0afbc3895d74d5be68df4e7ae2609234448f
Luis Miguens Fernandez <luis.miguens@gmail.com>
2017-11-11 19:12:39 +0100

    Some tries with layouts

--- HangmanForAndroid-2/app/src/main/java/io/github/luismiguensfernandez/MainActivity.java
+++ HangmanForAndroid-2/app/src/main/java/io/github/luismiguensfernandez/MainActivity.java
@@ -2,16 +2,21 @@ package io.github.luismiguensfernandez;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
 
+import java.util.SortedMap;
+
 public class MainActivity extends Activity{
 
     private TextView m_displayWord;
     private ImageView m_hangmanImage;
+    private SortedMap<Character, Button> m_keyboard;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +30,29 @@ public class MainActivity extends Activity{
         LinearLayout fullLayout = new LinearLayout(this);
         fullLayout.setLayoutParams(
                 new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         fullLayout.setOrientation(LinearLayout.VERTICAL);
+        fullLayout.setWeightSum(100.0f);
 
         LinearLayout topLayout = new LinearLayout(this);
         topLayout.setLayoutParams(
                 new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,
-                        1.0f));
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT,
+                        25.0f));
         topLayout.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout middleLayout = new LinearLayout(this);
         middleLayout.setLayoutParams(
                 new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,
-                        2.0f));
+                        50.0f));
         middleLayout.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout bottomLayout = new LinearLayout(this);
         bottomLayout.setLayoutParams(
                 new LinearLayout.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,
-                        1.0f));
+                        25.0f));
         bottomLayout.setOrientation(LinearLayout.VERTICAL);
 
         fullLayout.addView(topLayout);
@@ -54,6 +60,11 @@ public class MainActivity extends Activity{
         fullLayout.addView(bottomLayout);
 
         m_displayWord = new TextView(this);
+        m_displayWord.setGravity(Gravity.CENTER);
+        m_displayWord.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        //m_displayWord.setAutoSizeTextTypeUniformWithConfiguration(  TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
         TextView blabla2 =  new TextView(this);
         TextView blabla3 =  new TextView(this);
         blabla2.setText("xxxxxxxxxxxxxxxx");
